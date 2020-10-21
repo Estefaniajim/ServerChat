9a0b67b0bafe7c7580ff93d0122e41c76e736992
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2020-10-20 20:00:54 -0500

    Added comments and added imports
    Also added .gitignore

---
+++ chatserver.c
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <WS2tcpip.h>
+#pragma comment (lib, "w2_32.lib")
+using namespace std;
+void main()
+{
+	// Initialize winsock
+	// Create socket
+	// Bind the socket to an ip adress and port
+	// Tell windsock the socket is for listening
+	// Wait for a connection
+	// Close listening socket
+	// While loop: accept and echo message back to client
+	// Close the sock
+	// ShutDown winsock
+	}

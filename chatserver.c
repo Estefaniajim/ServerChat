56d052ae4af62c02966a4209087224c8dbb07546
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2020-10-20 20:31:47 -0500

    Update chatserver.c

--- chatserver.c
+++ chatserver.c
@@ -1,16 +1,147 @@
 #include <iostream>
 #include <WS2tcpip.h>
-#pragma comment (lib, "w2_32.lib")
+#include <string>
+#include <sstream>
+
+#pragma comment (lib, "ws2_32.lib")
+
 using namespace std;
+
 void main()
 {
-	// Initialize winsock
-	// Create socket
-	// Bind the socket to an ip adress and port
-	// Tell windsock the socket is for listening
-	// Wait for a connection
-	// Close listening socket
-	// While loop: accept and echo message back to client
-	// Close the sock
-	// ShutDown winsock
+	// Initialze winsock
+	// Creamos el winsock
+	WSADATA wsData;
+	WORD ver = MAKEWORD(2, 2);
+	int wsOk = WSAStartup(ver, &wsData);
+	if (wsOk != 0)
+	{
+		cerr << "Can't Initialize winsock! Quitting" << endl;
+		return;
 	}
+	// Create a socket
+	// Creamos un nuevo socket
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+	if (listening == INVALID_SOCKET)
+	{
+		cerr << "No podemos agregar mas usuarios" << endl;
+		return;
+	}
+	// Bind the ip address and port to a socket
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(54000);
+	hint.sin_addr.S_un.S_addr = INADDR_ANY;
+	bind(listening, (sockaddr*)&hint, sizeof(hint));
+	listen(listening, SOMAXCONN);
+	// Master file descriptor set
+	fd_set master;
+	FD_ZERO(&master);
+	// First socket
+	// Primer socket
+	FD_SET(listening, &master);
+	bool running = true; 
+	while (running)
+	{
+		//Copy of the master list
+		//Copia de la lsita de master
+		fd_set copy = master;
+
+		// Check the user that is writing in the server
+		//	Checamos que usuario esta escribiendo en el server
+		int socketCount = select(0, &copy, nullptr, nullptr, nullptr);
+
+		// Loop through all the current users
+		// Iteramos por todos los usuarios que tenemos activos
+		for (int i = 0; i < socketCount; i++)
+		{
+			SOCKET sock = copy.fd_array[i];
+			if (sock == listening)
+			{
+				// We accept a new user
+				SOCKET user = accept(listening, nullptr, nullptr);
+				//We addd the user to the list of users
+				FD_SET(user, &master);
+				// We display a message to the new user
+				string bienvenida = "Bienvenido al chat!\r\n";
+				send(client, bienvenida.c_str(), bienvenida.size() + 1, 0);
+			}
+			else
+			{
+				char buf[4096];
+				ZeroMemory(buf, 4096);
+				
+				// Server Receive message
+				//Servidor recibe un nuevo mensaje
+				int bytesIn = recv(sock, buf, 4096, 0);
+				if (bytesIn <= 0)
+				{
+					// Drop the client
+					// Le decimos bye al usuario
+					string despedida = "Bye!\r\n";
+					send(client, despedida.c_str(), despedida.size() + 1, 0);
+					closesocket(sock);
+					FD_CLR(sock, &master);
+				}
+				else
+				{
+					// Check to see if it's a command. \quit kills the server
+					if (buf[0] == '\\')
+					{
+						// Is the command quit? 
+						string cmd = string(buf, bytesIn);
+						if (cmd == "\\quit")
+						{
+							running = false;
+							break;
+						}
+
+						// Unknown command
+						continue;
+					}
+
+					// Send message to other clients, and definiately NOT the listening socket
+
+					for (int i = 0; i < master.fd_count; i++)
+					{
+						SOCKET outSock = master.fd_array[i];
+						if (outSock != listening && outSock != sock)
+						{
+							ostringstream ss;
+							ss << "SOCKET #" << sock << ": " << buf << "\r\n";
+							string strOut = ss.str();
+
+							send(outSock, strOut.c_str(), strOut.size() + 1, 0);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// Remove the listening socket from the master file descriptor set and close it
+	// to prevent anyone else trying to connect.
+	FD_CLR(listening, &master);
+	closesocket(listening);
+	
+	// Message to let users know what's happening.
+	string msg = "Server is shutting down. Goodbye\r\n";
+
+	while (master.fd_count > 0)
+	{
+		// Get the socket number
+		SOCKET sock = master.fd_array[0];
+
+		// Send the goodbye message
+		send(sock, msg.c_str(), msg.size() + 1, 0);
+
+		// Remove it from the master file list and close the socket
+		FD_CLR(sock, &master);
+		closesocket(sock);
+	}
+
+	// Cleanup winsock
+	WSACleanup();
+
+	system("pause");
+}

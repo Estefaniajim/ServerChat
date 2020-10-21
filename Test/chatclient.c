3835812ff930361333a3d61095f46902b3480bbb
Memohto <guillermohiroshi@gmail.com>
2020-10-21 14:10:21 +0000

    Manejo de usuarios

--- Test/chatclient.c
+++ Test/chatclient.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
-#include <sys/socket.h> 
 #include <stdlib.h>
-#include <netinet/in.h>
+#include <unistd.h>
 #include <string.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <pthread.h>
 
+#define MESSAGE_LEN 100
+#define NAME_LEN 50
 
-// Comando para compilar: gcc chatclient.c -o chatclient	
+// Comando para compilar: gcc chatclient.c -o chatclient -pthread
 int main(int argc, char **argv) {
 	if(argc != 4) {
-		printf("Uso: %s <serverIP> <port> <nickName>\n", argv[0]);
+		printf("Uso: %s <serverIP> <port> <nickname>\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 	
 	int port = atoi(argv[2]);
+	char name[NAME_LEN] = "";
+	strcpy(name, argv[3]);
 	
 	struct sockaddr_in server_addr;
 	server_addr.sin_family = AF_INET;
@@ -27,10 +33,18 @@ int main(int argc, char **argv) {
 		return EXIT_FAILURE;
 	};
 	
-	char message[100] = "";
+	printf("Bienvenido al chat\n");
+	
+	char message[MESSAGE_LEN] = "";
+	send(connectionfd, name, strlen(name), 0);
 	while(1) {
+		//TODO: Dividir entrada y salida en dos hilos
+		// if((read(connectionfd, message, MESSAGE_LEN) > 0)) {
+		// 	printf("Incoming message: %s", message);
+		// }
+		// bzero(message, MESSAGE_LEN);
 		printf("> ");
-		fgets(message, 100, stdin);
+		fgets(message, MESSAGE_LEN, stdin);
 		send(connectionfd, message, strlen(message), 0);
 	}
 }
--- Test/chatserver.c
+++ Test/chatserver.c
@@ -7,28 +7,85 @@
 #include <pthread.h>
 
 #define MAX_CLIENTS 5
+#define MESSAGE_LEN 100
+#define NAME_LEN 50
 
 typedef struct {
 	struct sockaddr_in address;
 	int sockfd;
 	int id;
+	char name[NAME_LEN];
 } client_t;
 
-char message[100] = "";
+client_t *clients[MAX_CLIENTS];
+char message[MESSAGE_LEN] = "";
+
+void add_client(client_t* client) {	
+	for(int i = 0; i < MAX_CLIENTS; i++) {
+		if(!clients[i]) {
+			clients[i] = client;
+			break;
+		}
+	}
+}
+
+void remove_client(client_t* client) {
+	for(int i = 0; i < MAX_CLIENTS; i++) {
+		if(clients[i]) {
+			if(clients[i] == client) {
+				printf("Usuario %s removido\n", client->name);
+				clients[i] = NULL;
+				break;
+			}
+		}
+	}
+}
+
+void broadcast_message(char *s, client_t* client) {	
+	for(int i = 0; i < MAX_CLIENTS; i++) {
+		if(clients[i]) {
+			printf("TEST: %s\n", clients[i]->name);
+			if(clients[i] != client) {
+				printf("Sending message to %s\n", clients[i]->name);
+				write(clients[i]->sockfd, s, strlen(s));
+			}
+		}
+	}
+}
 
 void *handle_client(void *arg) {
-	printf("Cliente se ha conectado!!\n");
+	int flag = 0;
 	client_t *client = (client_t*)arg;
-	while(recv(client->sockfd, message, 100, 0) > 0) {
-		if(strcmp(message, "bye") == 0) {
+	
+	if(read(client->sockfd, message, MESSAGE_LEN) > 0) {
+		printf("%s se ha unido\n", message);
+		strcpy(client->name, message);
+	} else {
+		printf("Error con el nickname!\n");
+		flag = 1;
+	};
+	bzero(message, MESSAGE_LEN);
+	
+	while(1) {
+		if(flag) {
 			break;
-		} else {
-			printf("Mensaje recibido: %s", message);
 		}
-		bzero(message, 100);
+		int receive = recv(client->sockfd, message, MESSAGE_LEN, 0);
+		if(receive > 0) {
+			printf("%s -> %s", client->name, message);
+			broadcast_message(message, client);
+		} else if(strcmp(message, "bye") == 0) {
+			printf("%s -> %s", client->name, message);
+			broadcast_message(message, client);
+			flag = 1;
+		} else {
+			flag = 1;
+		}
+		bzero(message, MESSAGE_LEN);
 	}
-	printf("Se acabó\n");
+	printf("%s se ha ido\n", client->name);
 	close(client->sockfd);
+	remove_client(client);
 	free(client);
 	pthread_detach(pthread_self());
 }
@@ -67,6 +124,7 @@ int main(int argc, char **argv) {
 		client->address = client_addr;
 		client->sockfd = connectionfd;
 		
+		add_client(client);
 		pthread_create(&thread, NULL, &handle_client, (void*)client);
 	}
 }
